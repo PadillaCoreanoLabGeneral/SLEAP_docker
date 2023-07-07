b19300e548fcd808706d4cbb21935583eadc3d27
padillacoreanolab <padillacoreanolab@gmail.com>
2023-07-07 17:04:19 -0400

    Update SLEAP.bat

--- SLEAP.bat
+++ SLEAP.bat
@@ -2,12 +2,26 @@
 
 echo Checking if Docker Desktop is installed...
 
-docker --version >nul 2>&1
+docker --versiona >nul 2>&1
 
 if %errorlevel% == 0 (
     echo Docker Desktop is already installed!
 ) else (
-    echo Docker Desktop is not installed. Installing...
-    choco install docker-desktop -y
+    echo Docker Desktop is not installed.
+    echo Please install Docker Dekstop
     echo Docker Desktop installed successfully!
 )
+
+echo Checking if Anaconda is installed...
+
+conda --version
+
+if %errorlevel% == 0 (
+    echo Conda is already installed!
+) else (
+    echo Conda is not installed. Installing...
+    echo Please install Anaconda/Conda/miniconda  
+    echo Anaconda installed successfully!
+)
+
+pause
